566532e3707421a285867957d88833d3bdba256b
moses-ramoeletsi <mramoeletsi14@gmail.com>
2025-02-27 15:10:24 +0200

    made changes on mongo link

--- backend/dbConfig/dbConnection.js
+++ backend/dbConfig/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL, {});
+    const conn = await mongoose.connect(process.env.MONGO_URI, {});
     console.log(`Database is connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
